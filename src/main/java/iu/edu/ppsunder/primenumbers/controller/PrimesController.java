dea02d1ed5901460ad66cd1fa0908671ab9d01e0
prajodh <prajodhpragaths@gmail.com>
2024-04-09 16:20:46 -0400

    added the authentication from practicum 13

--- src/main/java/iu/edu/ppsunder/primenumbers/controller/PrimesController.java
+++ src/main/java/iu/edu/ppsunder/primenumbers/controller/PrimesController.java
@@ -1,7 +1,10 @@
 package iu.edu.ppsunder.primenumbers.controller;
 
+import iu.edu.ppsunder.primenumbers.rabbitmq.MQSender;
 import iu.edu.ppsunder.primenumbers.service.IPrimesService;
 import iu.edu.ppsunder.primenumbers.service.PrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,14 +12,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     IPrimesService primesService;
-    public PrimesController(IPrimesService primesService){
+//    public PrimesController(IPrimesService primesService){
+//
+//        this.primesService = primesService;
+//
+//    }
+    private final MQSender mqSender;
 
+    public PrimesController(IPrimesService primesService, MQSender mqSender)
+    {
         this.primesService = primesService;
-
+        this.mqSender = mqSender;
     }
 
+
+//    @GetMapping("/{n}")
+//    public boolean isPrime(@PathVariable int n){
+//        return primesService.isPrime(n);
+//    }
     @GetMapping("/{n}")
-    public boolean isPrime(@PathVariable int n){
-        return primesService.isPrime(n);
+    public boolean isPrime(@PathVariable int n) {
+        boolean result = primesService.isPrime(n);
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username, n, result);
+        return result;
     }
+
 }
